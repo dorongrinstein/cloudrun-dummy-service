a6983dfba87ac326c3ec5d04de64422bb37dc62a
Doron <doron@buywiser.com>
2019-08-27 15:55:37 -0700

    added app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/dummy', (req, res) => res.json({"message": "dummy service"}));
+app.get('/dummy', (req, res) => {
+    console.log('request to dummy received');
+    res.json({"message": "dummy service"});
+});
 app.get('/', (req, res) => res.json({"message": "ready"}));
 app.listen(8080, () => console.log('listening on 8080'));
